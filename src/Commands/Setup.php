9a2fb6446c2f1439ccd057aa77441ce21f40554d
leganz <manuel.pirker-ihl@sushi.dev>
2025-03-07 23:31:50 +0100

    chore: work on the setup command

--- src/Commands/Setup.php
+++ src/Commands/Setup.php
@@ -5,10 +5,15 @@ namespace SushidevTeam\Fairu\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
 use Statamic\Console\RunsInPlease;
+use Statamic\Facades\Asset;
+use Statamic\Facades\AssetContainer as FacadesAssetContainer;
 use SushidevTeam\Fairu\Services\Fairu as ServicesFairu;
 
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\select;
+
+use Ramsey\Uuid\Uuid;
+use Statamic\Contracts\Assets\Asset as AssetsAsset;
 
 class Setup extends Command
 {
@@ -18,13 +23,17 @@ class Setup extends Command
 
     protected $description = 'Step by step wizard to bring your files to fairu';
 
+    protected ?array $credentials = null;
+    protected ?string $connection = null;
+
     public function handle(): void
     {
         $this->checkConnection();
         $this->importFiles();
     }
 
-    protected function checkConnection():void {
+    protected function checkConnection(): void
+    {
 
         $connection = select(
             label: 'What connection do you want to use?',
@@ -32,27 +41,61 @@ class Setup extends Command
             default: 'default'
         );
 
-        $credentials = config('fairu.connections.'.$connection);
+        $this->credentials = config('fairu.connections.' . $connection);
+        $this->connection = $connection;
 
-        $result = (new ServicesFairu($connection))->scope();
+        $result = (new ServicesFairu($connection))->getScopeFromEndpoint();
 
-        if ($result == null){
+        if ($result == null) {
             $this->error('Cannot check the given credentials. Please make sure you have set the environment keys "FAIRU_TENANT" and "FAIRU_TENANT_SECRET" or that you defined your custom connection in the configuration.');
         }
 
+        // Output some information about the keys
         $this->info("Your api keys are valid and we could recieve the following user data associated with the api.");
-
         $this->table(['id', 'email'], [Arr::only($result, ['id', 'email'])]);
 
-        confirm("Want to continue with the import of your files?");
+        $continue = confirm("Want to continue with the import of your files to fairu?");
 
+        if ($continue == false) {
+            exit;
+        }
     }
 
-    protected function importFiles(){
+    protected function importFiles()
+    {
 
-        // TODO: write a function that checks if the asset container is 
-        // an s3 bucket of plain statamic flat file system.
+        $containers = FacadesAssetContainer::all()?->pluck('handle')->toArray();
 
+        $assetContainer = select(
+            label: 'What container do you want to import?',
+            options: $containers,
+            default: Arr::first($containers),
+        );
+
+        $assets = Asset::whereContainer($assetContainer);
+
+        $assets->each(function ($asset) {
+            $uuid = (new ServicesFairu($this->connection))->convertToUuid($asset->id);
+            $this->importAssetToFairu($asset, $uuid);
+        });
+
+        $restart = confirm(
+            label: 'Do you want to import another container?',
+            default: false,
+            yes: 'Yes',
+            no: 'No',
+        );
+
+        if ($restart == false) {
+            $this->info("Setup for has been finished. Open https://docs.fairu.app/docs/statamic for further instructions.");
+            return;
+        }
+
+        return $this->importFiles();
     }
 
+    protected function importAssetToFairu(AssetsAsset $asset, $uuid)
+    {
+        // TODO: Write import logic
+    }
 }
--- src/Services/Fairu.php
+++ src/Services/Fairu.php
@@ -3,14 +3,19 @@
 namespace SushidevTeam\Fairu\Services;
 
 use Illuminate\Support\Facades\Http;
+use Ramsey\Uuid\Uuid;
 
 class Fairu {
 
     public \Illuminate\Http\Client\PendingRequest $client;
 
+    protected ?array $credentials = null;
+
     public function __construct(?string $connection = 'default')
     {
         $credentials = config('fairu.connections.'.$connection);
+
+        $this->credentials = $credentials;
 
         $this->client = Http::withHeaders([
             'Tenant' => data_get($credentials, 'tenant'),
@@ -22,11 +27,15 @@ class Fairu {
         return config('fairu.url') . "/$path";
     }
     
-    public function scope(): ?array {
+    public function getScopeFromEndpoint(): ?array {
 
         $result = $this->client->get($this->endpoint('api/users/scope'));
         return $result->json();
 
+    }
+
+    public function convertToUuid(string $str): string{
+        return Uuid::uuid5(Uuid::NAMESPACE_DNS, data_get($this->credentials, 'tenant') . $str)->toString();
     }
 
 }
