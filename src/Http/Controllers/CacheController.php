b1d6feabb8a7c977fd92329dad46ec449c37312b
leganz <manuel.pirker-ihl@sushi.dev>
2025-03-08 00:32:12 +0100

    fix: remove default utility

---
+++ src/Http/Controllers/CacheController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SushidevTeam\Fairu\Http\Controllers;
+
+class CacheController extends \Statamic\Http\Controllers\CP\Utilities\CacheController
+{
+    public function index()
+    {
+        return view('fairu::cache', [
+            'stache' => $this->getStacheStats(),
+            'cache' => $this->getApplicationCacheStats(),
+            'static' => $this->getStaticCacheStats(),
+            'images' => $this->getImageCacheStats(),
+        ]);
+    }
+
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace SushidevTeam\Fairu;
 
+use Illuminate\Support\Facades\View;
 use Statamic\Providers\AddonServiceProvider;
+use Statamic\Facades\Utility;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -11,12 +13,19 @@ class ServiceProvider extends AddonServiceProvider
     ];
 
     protected $routes = [
-        'cp' => __DIR__.'/../routes/cp.php',
-        'web' => __DIR__.'/../routes/web.php',
+        'cp' => __DIR__ . '/../routes/cp.php',
+        'web' => __DIR__ . '/../routes/web.php',
     ];
 
     public function bootAddon()
     {
-        
+        $packageName = str_replace('\\', '-', strtolower(__NAMESPACE__)); // z. B. "fairu-statamic"
+
+        View::addNamespace('fairu-statamic', base_path("resources/views/vendor/{$packageName}"));
+    }
+
+    public function register()
+    {
+        $this->app->bind(\Statamic\Http\Controllers\CP\Utilities\CacheController::class, \SushidevTeam\Fairu\Http\Controllers\CacheController::class);
     }
 }
