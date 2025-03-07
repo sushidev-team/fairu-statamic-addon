fdfcfac4799ba6a633b8c46ee2cc612574e0b948
leganz <manuel.pirker-ihl@sushi.dev>
2025-03-07 22:23:54 +0100

    fix: init commit for basic structure

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SushidevTeam\Fairu;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
