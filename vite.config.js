c44b8d949d52393e060ddd165b6c4839f29531cc
Martin Reitschmied <martin.reitschmied@sushi.dev>
2025-03-19 22:48:47 +0100

    chore: cleanup

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,22 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    const { host, protocol } = new URL(env.APP_URL ?? 'http://localhost');
-    console.log({ host, protocol });
+  const env = loadEnv(mode, process.cwd(), '');
+  const { host, protocol } = new URL(env.APP_URL ?? 'http://localhost');
 
-    return {
-        plugins: [
-            laravel({
-                input: ['resources/js/cp.js', 'resources/css/cp.css'],
-                refresh: true,
-                detectTls: protocol.startsWith('https') ? host : null,
-                publicDirectory: 'resources/dist',
-                hotFile: 'resources/dist/hot',
-            }),
-            vue(),
-        ],
-        server: {
-            host,
-        },
-    };
+  return {
+    plugins: [
+      laravel({
+        input: ['resources/js/cp.js', 'resources/css/cp.css'],
+        refresh: true,
+        detectTls: protocol.startsWith('https') ? host : null,
+        publicDirectory: 'resources/dist',
+        hotFile: 'resources/dist/hot',
+      }),
+      vue(),
+    ],
+    server: {
+      host,
+    },
+  };
 });
