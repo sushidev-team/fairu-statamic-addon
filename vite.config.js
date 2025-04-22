a0aa6fdb596d57666548977e366d9c325a929549
Martin Reitschmied <martin.reitschmied@sushi.dev>
2025-04-22 11:05:58 +0200

    fix: correct base path for vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const { host, protocol } = new URL(env.APP_URL ?? 'http://localhost');
 
   return {
-    base: mode === 'production' ? '/vendor/fairu-statamic/' : '/',
+    base: mode === 'production' ? '/vendor/fairu-statamic/build/' : '/',
     plugins: [
       laravel({
         input: ['resources/js/cp.js', 'resources/css/cp.css'],
